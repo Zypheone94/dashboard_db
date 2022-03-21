ada6e566e875250d469fb625eb49fe6a1a840808
Zypheone94 <maximerene.durand.pro@gmail.com>
2022-03-21 18:42:42 +0100

    side bar profil part
    Ajout de la partie profil dans la barre latérale, Ajout du nom, image et Param

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -6,6 +6,7 @@ import {
     Route,
     Link
   } from "react-router-dom";
+import bg_test from './assets/bg_test.png'
 
 function App() {
 
@@ -14,6 +15,20 @@ function App() {
       <section id="sideBar">
         <div id="title_sideBar"></div>
         <div id="content_sideBar">
+          <div id="userLink">
+            <div>
+              <img src={bg_test} alt="profil_pic" />
+              <div id="user_content">
+                <h3 id="user_name">Name</h3>
+                <p id="user_title">Super Admin</p>
+              </div>
+            </div>
+            <svg width="15" height="18" viewBox="0 0 4 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M2 10C2.55228 10 3 9.55228 3 9C3 8.44772 2.55228 8 2 8C1.44772 8 1 8.44772 1 9C1 9.55228 1.44772 10 2 10Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M2 3C2.55228 3 3 2.55228 3 2C3 1.44772 2.55228 1 2 1C1.44772 1 1 1.44772 1 2C1 2.55228 1.44772 3 2 3Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M2 17C2.55228 17 3 16.5523 3 16C3 15.4477 2.55228 15 2 15C1.44772 15 1 15.4477 1 16C1 16.5523 1.44772 17 2 17Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
+          </div>
           <Router>
             <Link to="/">
               <div className="link_router">
